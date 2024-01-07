097e754259b4a14fb5b7e9d54e6a855d01adf0bd
Faustas Linkevičius <faulin@ktu.lt>
2024-01-07 20:25:12 +0200

    Site

--- Saitynai/urls.py
+++ Saitynai/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("api/", include("skelbiameAPI.urls")),
-    path("", include("website.urls"))
+    path("site/", include("website.urls"))
 ]
