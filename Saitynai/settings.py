b5c91a4f0d105214392412eb9633780d9bb7191c
Faustas Linkevičius <faulin@ktu.lt>
2024-01-07 20:14:05 +0200

    maybe fix for azure again

--- Saitynai/settings.py
+++ Saitynai/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["skelbiameapi.azurewebsites.net", "localhost", "127.0.0.1"]
 # Application definition
 
 INSTALLED_APPS = [
+    'website',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'skelbiameAPI',
-    'website',
     'corsheaders',
 ]
 
